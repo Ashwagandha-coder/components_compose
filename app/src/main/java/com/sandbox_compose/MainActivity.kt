ebfaf973f278e218ef29d254eeeeca47085db532
Ashawagandha-coder <max.cacz@yandex.ru>
2022-11-17 16:20:27 +0500

    add more picture and padding in 8 dp in container

--- app/src/main/java/com/sandbox_compose/MainActivity.kt
+++ app/src/main/java/com/sandbox_compose/MainActivity.kt
@@ -7,12 +7,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sandbox_compose.ui.theme.Sandbox_ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -58,10 +61,10 @@ data class MessageCompose(
 @Composable
 fun MessageCard(message: MessageCompose) {
 
-    Row {
+    Row(modifier = Modifier.padding(8.dp)) {
 
         Image(
-            painter = painterResource(id = R.drawable.ic_other_profile_foreground),
+            painter = painterResource(id = R.drawable.ic_launcher_background),
             contentDescription = "Profile people"
         )
 
