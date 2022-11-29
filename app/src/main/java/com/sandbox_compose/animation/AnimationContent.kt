db35e3123fcf341e3da4503ca2cc083d766d7d97
Ashawagandha-coder <max.cacz@yandex.ru>
2022-11-29 18:07:57 +0500

    add listener in button

--- app/src/main/java/com/sandbox_compose/animation/AnimationContent.kt
+++ app/src/main/java/com/sandbox_compose/animation/AnimationContent.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -32,14 +33,19 @@ class AnimationContent : ComponentActivity() {
             mutableStateOf(false)
         }
 
-        Column(modifier = Modifier.size(200.dp)) {
+        Column(modifier = Modifier.fillMaxSize()) {
 
             Text(text = "Hello from Animation Content", color = Color.Black)
+
+            Button(onClick = )
 
         }
 
 
     }
+
+
+    private fun onClickButton(expandable: MutableState<Boolean>) { expandable.value = !expandable.value }
 
 
 
