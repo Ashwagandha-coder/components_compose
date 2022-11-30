bb2e3c9c278fc29a71b12c3acf1e42bdea535db2
Ashawagandha-coder <max.cacz@yandex.ru>
2022-11-30 18:25:18 +0500

    [anim] save state work with Crossfade.kt

--- app/src/main/java/com/sandbox_compose/animation/Crossfade.kt
+++ app/src/main/java/com/sandbox_compose/animation/Crossfade.kt
@@ -3,6 +3,7 @@ package com.sandbox_compose.animation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,8 +11,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposeCompilerApi
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,10 +36,26 @@ class Crossfade : ComponentActivity() {
     @Composable
     fun mCrossFade() {
 
+
+        var state by remember {
+            mutableStateOf(true)
+        }
+
+
+
+        Crossfade(targetState = state) { screen ->
+
+            when(screen) {
+
+
+
+            }
+
+        }
+
         Column(modifier = Modifier.fillMaxSize()) {
 
             Image(painter = painterResource(id = R.drawable.monitor_pic ), contentDescription = "")
-
 
 
             Button(onClick = {}) {
@@ -49,6 +65,24 @@ class Crossfade : ComponentActivity() {
         }
 
 
+
+
+    }
+
+
+
+    @Composable
+    private fun someImage() {
+
+        Image(painter = painterResource(id = R.drawable.ic_launcher_svg_pic), contentDescription = "do Some" )
+
+
+    }
+
+    @Composable
+    private fun someText() {
+
+        Text(text = "Some text")
 
 
     }
---
+++ app/src/main/java/com/sandbox_compose/animation/State.kt
@@ -0,0 +1,11 @@
+package com.sandbox_compose.animation
+
+enum class State(val flag: Boolean) {
+
+
+    EXPANDED(true),
+    COLLAPSED(true),
+    IMAGE(true),
+    TEXT(true)
+
+}
