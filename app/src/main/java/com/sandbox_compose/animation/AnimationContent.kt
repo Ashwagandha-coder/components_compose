fe954cc8b988b706bb9ec78063b0235a20322ae7
Ashawagandha-coder <max.cacz@yandex.ru>
2022-11-30 14:38:51 +0500

    [anim] work with full AnimateContentApi

--- app/src/main/java/com/sandbox_compose/animation/AnimationContent.kt
+++ app/src/main/java/com/sandbox_compose/animation/AnimationContent.kt
@@ -5,16 +5,19 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -25,8 +28,7 @@ import androidx.compose.ui.unit.dp
 
 class AnimationContent : ComponentActivity() {
 
-    private lateinit var tv: TextView
-
+    private val time: Int = 1500
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,12 +63,12 @@ class AnimationContent : ComponentActivity() {
                 contentDescription = "do Some",
                 modifier = Modifier
                     .background(Color.Yellow)
-                    .animateContentSize(tween(1500))
+                    .animateContentSize(tween(time))
             )
 
             Button(onClick = { onClickButton(expandable = expandable) },
-                modifier = Modifier.padding(top = 20.dp)) {
-                Text(text = if (expandable.value) "Hide" else "Show")
+                modifier = Modifier.padding(top = 10.dp)) {
+                Text(text = if (expandable.value) "Show" else "Hide")
             }
 
         }
@@ -83,7 +85,38 @@ class AnimationContent : ComponentActivity() {
 
     }
 
+
+    private fun onClickButton(expandable: MutableState<Boolean>) { expandable.value = !expandable.value }
+
+
+
+
+    @Composable
+    private fun AnimateContentAPI() {
+
+        Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
+
+            AnimatedContent(targetState = ) {
+
+
+
+            }
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+    @Preview(showBackground = true)
+    @Composable
+    private fun PreviewAnimateCotentAPI() {}
+
 }
-
-
-private fun onClickButton(expandable: MutableState<Boolean>) { expandable.value = !expandable.value }
