b605aa7574700e49cb862df0311c3f053f4dd791
Ashawagandha-coder <max.cacz@yandex.ru>
2022-12-02 16:12:07 +0500

    save state

--- app/src/main/java/com/sandbox_compose/animation/ActivityForAnimationVisibility.kt
+++ app/src/main/java/com/sandbox_compose/animation/ActivityForAnimationVisibility.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.*
 import androidx.compose.animation.core.*
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -14,8 +15,10 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.sandbox_compose.R
 import com.sandbox_compose.ui.theme.Sandbox_ComposeTheme
 
 class ActivityForAnimation : ComponentActivity() {
@@ -123,4 +126,29 @@ fun textForTest() {
         Text(text = "Hello Comp!", color = Color.Black)
 
     }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun AnimatedVisibility() {
+
+
+    val visible = true
+
+    AnimatedVisibility(
+        visible = visible,
+        enter = slideInHorizontally() + expandHorizontally(expandFrom = Alignment.End)
+                + fadeIn(),
+        exit = slideOutHorizontally(targetOffsetX = { fullWidth -> fullWidth })
+                + shrinkHorizontally() + fadeOut(),
+    ) {
+        Image(
+            modifier = Modifier.fillMaxWidth(),
+            painter = painterResource(id = R.drawable.ic_launcher_svg_pic),
+            contentDescription = "")
+
+
+    }
+
 }
