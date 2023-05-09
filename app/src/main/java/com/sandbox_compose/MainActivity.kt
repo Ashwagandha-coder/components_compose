8360804357e195060bc4477c96b2b03de6a71d5a
Danil M <58209188+ashwagandha-coder@users.noreply.github.com>
2023-05-09 22:51:21 +0300

    refactor code..

--- app/src/main/java/com/sandbox_compose/MainActivity.kt
+++ app/src/main/java/com/sandbox_compose/MainActivity.kt
@@ -1,25 +1,10 @@
 package com.sandbox_compose
 
-import android.content.res.Configuration
 import android.os.Bundle
-import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Checkbox
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.sandbox_compose.func.PreviewMessageCard_3
 import com.sandbox_compose.ui.theme.Sandbox_ComposeTheme
 
@@ -27,7 +12,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            
+
             Sandbox_ComposeTheme() {
                 androidx.compose.material.Surface(modifier = Modifier.fillMaxSize()) {
                     PreviewMessageCard_3()
